4d01a5fbeccd3f1673b07c711eec946fce2ca555
Dorian-Garcia-pro <garcia.dorian.pro@gmail.com>
2022-03-17 22:55:40 +0100

    docu

--- src/screens/Documentation.js
+++ src/screens/Documentation.js
@@ -4,34 +4,27 @@ import Navbar from "../components/Navbar";
 import pwpt1 from "../assets/pwpt/1LesFondamentaux.png";
 
 const Documentation = () => {
-
   const linkToPPTFile = "https://slideplayer.fr/slide/12703519/";
 
   return (
     <div className="documentation-page">
       <Navbar />
       <h1 className="titre-h1-fondnoir">
-        DOCUMENTATION SUR LA PREVENTION & SECURITE
+        DOCUMENTATION SUR LA PRÉVENTION & SECURITÉ
       </h1>
 
       <div className="documentationContainer">
         <div className="documentation-conteneur">
-          <h2 className="titre-bloc-intro">Titre de présentation</h2>
+          {/*   <h2 className="titre-bloc-intro">Titre de présentation</h2> */}
           <div className="bloc-intro-doc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus odit
-            est harum recusandae, tempora beatae incidunt assumenda sequi dolore
-            nesciunt deleniti excepturi fuga id at inventore, facilis, non
-            vitae? Quas. Excepturi dolores atque ipsum facilis quod commodi
-            blanditiis ducimus. Reprehenderit culpa ea quaerat, ut incidunt
-            dolorem veniam! Omnis consequatur facilis dolorum, fugiat fuga hic
-            reprehenderit expedita, enim saepe optio quo? Ipsam fugit quos
-            minima nisi dolor pariatur, natus doloribus possimus corrupti vitae,
-            quis ex, atque blanditiis dolores cumque magni iusto modi
-            accusantium? Quae deserunt dolorem perferendis voluptatibus
-            blanditiis ea delectus. Atque commodi cum consectetur vel ipsum odit
-            praesentium quasi itaque rem optio quidem, aliquid laborum quo ipsa
-            obcaecati. Iusto quidem modi neque accusamus nesciunt eius
-            recusandae, labore ea expedita velit.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />{" "}
+            <br /> Natus odit est harum recusandae, tempora beatae incidunt
+            assumenda sequi dolore nesciunt deleniti excepturi fuga id at
+            inventore, facilis, non vitae? Quas. Excepturi dolores atque ipsum
+            facilis quod commodi blanditiis ducimus. Reprehenderit culpa ea
+            quaerat, ut incidunt dolorem veniam! Omnis consequatur facilis
+            dolorum, fugiat fuga hic reprehenderit expedita, enim saepe optio
+            quo?
           </div>
           <div className="liste-cards">
             <div className="card-pwpt">
@@ -123,16 +116,15 @@ const Documentation = () => {
             </div>
           </div>
 
-          TEST LECTURE PWPT
-<br />
-<iframe
-        src={`https://view.officeapps.live.com/op/embed.aspx?src=${linkToPPTFile}`}
-        width="100%"
-        height="600px"
-        frameBorder="0"
-        title="slides"
-      ></iframe>
-
+          {/*       TEST LECTURE PWPT */}
+          <br />
+          <iframe
+            src={`https://view.officeapps.live.com/op/embed.aspx?src=${linkToPPTFile}`}
+            width="100%"
+            height="600px"
+            frameBorder="0"
+            title="slides"
+          ></iframe>
         </div>
       </div>
     </div>
