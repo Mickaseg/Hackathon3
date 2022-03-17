e0f3ae1f265a8597da6bab564f180ca45e8432d7
Mickaseg <mickael_seguin@hotmail.fr>
2022-03-17 08:59:51 +0100

    splited quizz into 2 components

---
+++ src/components/QuizzTheme1.js
@@ -0,0 +1,85 @@
+import axios from "axios";
+import "./components-styles/QuizzDiv.css";
+import Reponse from "./Reponse";
+import { useState, useEffect } from "react";
+const QuizzTheme1 = ({ questionsTheme1 }) => {
+  const [answers, setAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [endQuizz, setEndQuizz] = useState(false);
+  const [currentQuestionNumber, setCurrentQuestionNumber] = useState(1);
+  const [currentQuestion, setCurrentQuestion] = useState([]);
+
+  const getAnswers = () => {
+    axios
+      .get(
+        `${process.env.REACT_APP_BACK}/quizz/questions/${currentQuestionNumber}/answers`
+      )
+      .then((res) => res.data)
+      .then((data) => setAnswers(data));
+  };
+
+  const getCurrentQuestion = () => {
+    axios
+      .get(
+        `${process.env.REACT_APP_BACK}/quizz/questions/${currentQuestionNumber}`
+      )
+      .then((res) => res.data)
+      .then((data) => setCurrentQuestion(data));
+  };
+
+  const nextQuestion = currentQuestionNumber + 1;
+
+  const setNextQuestion = () => {
+    setCurrentQuestionNumber(nextQuestion);
+  };
+
+  const handleChoice = (isCorrect) => {
+    if (isCorrect === 1) {
+      setScore(score + 1);
+      setNextQuestion();
+    } else if (isCorrect === 1 && nextQuestion > questionsTheme1.length) {
+      setScore(score + 1);
+      setEndQuizz(true);
+    } else if (nextQuestion > questionsTheme1.length) {
+      setEndQuizz(true);
+    } else {
+      setNextQuestion();
+    }
+  };
+
+  useEffect(() => {
+    getAnswers();
+  }, [currentQuestionNumber, endQuizz]);
+
+  console.log(answers);
+  return (
+    <>
+      {endQuizz ? (
+        <div>FIN</div>
+      ) : (
+        <div className="quizzDiv">
+          {currentQuestionNumber}
+          <img
+            src="https://www.metiers-electricite.com/wp-content/uploads/2018/05/Installateur-electricien-metier-650x-650x350.jpg"
+            alt=""
+            className="img"
+          />{" "}
+          {/* {console.log("POULET", question)} */}
+          <div>QUESTION : {currentQuestion.map((el) => el.question_text)}</div>
+          <div className="answerContainer">
+            {answers.map((el) => (
+              <div
+                className="answer"
+                onClick={() => handleChoice(el.isCorrect)}
+              >
+                {el.answer_text}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default QuizzTheme1;
--- src/screens/Quizz.js
+++ src/screens/Quizz.js
@@ -1,35 +1,51 @@
 import "./screens-styles/Quizz.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import QuizzDiv from "../components/QuizzDiv";
+import QuizzTheme2 from "../components/QuizzTheme2";
+import QuizzTheme1 from "../components/QuizzTheme1";
 
 const Quizz = () => {
   const [currentQuestion, setCurrentQuestion] = useState([]);
-  const [currentQuestionNumber, setCurrentQuestionNumber] = useState(1);
+  // const [currentQuestionNumber, setCurrentQuestionNumber] = useState(1);
+  const [questionsTheme1, setQuestionsTheme1] = useState([]);
+  const [questionsTheme2, setQuestionsTheme2] = useState([]);
   console.log(currentQuestion);
 
-  const getCurrentQuestion = () => {
+  const getQuestionsTheme1 = () => {
     axios
-      .get(
-        `${process.env.REACT_APP_BACK}/quizz/questions/${currentQuestionNumber}`
-      )
+      .get(`${process.env.REACT_APP_BACK}/quizz/questions/theme1`)
       .then((res) => res.data)
-      .then((data) => setCurrentQuestion(data));
+      .then((data) => setQuestionsTheme1(data));
+  };
+  console.log(questionsTheme1);
+  const getQuestionsTheme2 = () => {
+    axios
+      .get(`${process.env.REACT_APP_BACK}/quizz/questions/theme2`)
+      .then((res) => res.data)
+      .then((data) => setQuestionsTheme2(data));
   };
 
+  // const getCurrentQuestion = () => {
+  //   axios
+  //     .get(
+  //       `${process.env.REACT_APP_BACK}/quizz/questions/${currentQuestionNumber}`
+  //     )
+  //     .then((res) => res.data)
+  //     .then((data) => setCurrentQuestion(data));
+  // };
+
   useEffect(() => {
+    getQuestionsTheme1();
+    getQuestionsTheme2();
     getCurrentQuestion();
-  }, [currentQuestionNumber]);
+  }, []);
 
   return (
     <div className="quizz">
       <h1>QUIZZ</h1>
       <div className="quizzContainer">
-        <QuizzDiv
-          question={currentQuestion}
-          currentQuestionNumber={currentQuestionNumber}
-          setCurrentQuestionNumber={setCurrentQuestionNumber}
-        />
+        <QuizzTheme1 questionTheme1={questionsTheme1} />
+        <QuizzTheme2 questionsTheme2={questionsTheme2} />
       </div>
     </div>
   );
