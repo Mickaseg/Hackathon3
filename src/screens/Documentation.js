576bd1cf82075540f5679c426cbde6b9d25f40ae
Marsaleix Romain <msx.coach@gmail.com>
2022-03-18 03:09:53 +0100

    page documents ok

--- src/screens/Documentation.js
+++ src/screens/Documentation.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./screens-styles/Documentation.css";
 import Navbar from "../components/Navbar";
-import pwpt1 from "../assets/pwpt/1LesFondamentaux.png";
+import Cartedynam from "../components/Cartedynam";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Documentation = () => {
-
-  const linkToPPTFile = "https://slideplayer.fr/slide/12703519/";
+  const [card, setCard] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BACK}/carddetails`)
+      .then((res) => res.data)
+      .then((data) => setCard(data));
+  }, []);
 
   return (
     <div className="documentation-page">
-      <Navbar />
+      <Navbar /> {console.log(card)}
       <h1 className="titre-h1-fondnoir">
-        DOCUMENTATION SUR LA PREVENTION & SECURITE
+        DOCUMENTS TELECHARGEABLES SUR LA PREVENTION & SECURITE AU TRAVAIL
       </h1>
-
       <div className="documentationContainer">
         <div className="documentation-conteneur">
           <h2 className="titre-bloc-intro">Titre de présentation</h2>
@@ -34,105 +40,10 @@ const Documentation = () => {
             recusandae, labore ea expedita velit.
           </div>
           <div className="liste-cards">
-            <div className="card-pwpt">
-              <img src={pwpt1} alt="" className="img-card" />
-              <div className="card-blocText">
-                <h3 className="title-card">Titre de l'article</h3>
-                <p className="p-card">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit
-                  lorem, ipsum dolor..
-                </p>
-              </div>
-            </div>
-
-            <div className="card-pwpt">
-              <img src={pwpt1} alt="" className="img-card" />
-              <div className="card-blocText">
-                <h3 className="title-card">Titre de l'article</h3>
-                <p className="p-card">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit
-                  lorem, ipsum dolor..
-                </p>
-              </div>
-            </div>
-
-            <div className="card-pwpt">
-              <img src={pwpt1} alt="" className="img-card" />
-              <div className="card-blocText">
-                <h3 className="title-card">Titre de l'article</h3>
-                <p className="p-card">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit
-                  lorem, ipsum dolor..
-                </p>
-              </div>
-            </div>
-
-            <div className="card-pwpt">
-              <img src={pwpt1} alt="" className="img-card" />
-              <div className="card-blocText">
-                <h3 className="title-card">Titre de l'article</h3>
-                <p className="p-card">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit
-                  lorem, ipsum dolor..
-                </p>
-              </div>
-            </div>
-
-            <div className="card-pwpt">
-              <img src={pwpt1} alt="" className="img-card" />
-              <div className="card-blocText">
-                <h3 className="title-card">Titre de l'article</h3>
-                <p className="p-card">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit
-                  lorem, ipsum dolor..
-                </p>
-              </div>
-            </div>
-
-            <div className="card-pwpt">
-              <img src={pwpt1} alt="" className="img-card" />
-              <div className="card-blocText">
-                <h3 className="title-card">Titre de l'article</h3>
-                <p className="p-card">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit
-                  lorem, ipsum dolor..
-                </p>
-              </div>
-            </div>
-
-            <div className="card-pwpt">
-              <img src={pwpt1} alt="" className="img-card" />
-              <div className="card-blocText">
-                <h3 className="title-card">Titre de l'article</h3>
-                <p className="p-card">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit
-                  lorem, ipsum dolor..
-                </p>
-              </div>
-            </div>
-
-            <div className="card-pwpt">
-              <img src={pwpt1} alt="" className="img-card" />
-              <div className="card-blocText">
-                <h3 className="title-card">Titre de l'article</h3>
-                <p className="p-card">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit
-                  lorem, ipsum dolor..
-                </p>
-              </div>
-            </div>
+            {card.map((el, index) => (
+              <Cartedynam donnees={el} key={index} />
+            ))}
           </div>
-
-          TEST LECTURE PWPT
-<br />
-<iframe
-        src={`https://view.officeapps.live.com/op/embed.aspx?src=${linkToPPTFile}`}
-        width="100%"
-        height="600px"
-        frameBorder="0"
-        title="slides"
-      ></iframe>
-
         </div>
       </div>
     </div>
